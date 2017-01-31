4364df40b38a588238d14e1bc5d8ad7411c0187a
Clint Kolodziej <clint@clintkolodziej.com>
2017-01-31 16:39:35 -0600

    Added dynamic galleries (with demo data)

--- src/avalonbuild.com/Controllers/HomeController.cs
+++ src/avalonbuild.com/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using avalonbuild.com.Models;
+using avalonbuild.com.Models.Galleries;
 
 namespace avalonbuild.com.Controllers
 {
@@ -19,13 +22,36 @@ namespace avalonbuild.com.Controllers
         [Route("/galleries")]
         public IActionResult Galleries()
         {
-            return View();
+            List<Gallery> galleries = new List<Gallery>()
+            {
+                new Gallery {Name="Interiors", Url= "/gallery/interiors", ImageUrl="~/images/galleries/interiors.jpg"},
+                new Gallery {Name="Exteriors", Url= "/gallery/exteriors", ImageUrl="~/images/galleries/exteriors.jpg"},
+                new Gallery {Name="Details", Url= "/gallery/details", ImageUrl="~/images/galleries/details.jpg"},
+                new Gallery {Name="Before and After", Url= "/gallery/before-and-after", ImageUrl="~/images/galleries/before-and-after.jpg"}
+            };
+
+            return View(galleries);
         }
 
         [Route("/gallery/{name}")]
         public IActionResult Gallery(string name)
         {
-            return View(name);
+            Gallery gallery = new Gallery()
+            {
+                Name = name,
+                Url = "/gallery/" + name,
+                Images = new List<Image>()
+                {
+                    new Image {Name="One", ImageUrl="~/images/gallery/b25-1.JPG"},
+                    new Image {Name="Two", ImageUrl="~/images/gallery/b38.JPG"},
+                    new Image {Name="Three", ImageUrl="~/images/gallery/b47.JPG"},
+                    new Image {Name="Four", ImageUrl="~/images/gallery/b55.JPG"},
+                    new Image {Name="Five", ImageUrl="~/images/gallery/c6.JPG"},
+                    new Image {Name="Six", ImageUrl="~/images/gallery/c33.JPG"}
+                }
+            };
+
+            return View(gallery);
         }
 
         [Route("/referrals")]
---
+++ src/avalonbuild.com/Models/Galleries/Image.cs
@@ -0,0 +1,8 @@
+namespace avalonbuild.com.Models.Galleries
+{
+	public class Image
+	{
+		public string Name { get; set; }
+		public string ImageUrl { get; set; }
+	}
+}
