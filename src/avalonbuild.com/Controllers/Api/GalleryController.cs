f4e4b1d9cdb88449d47e920c71a2360421c840ae
Clint Kolodziej <clint@clintkolodziej.com>
2017-04-08 13:45:24 -0500

    Functional gallery editing

--- src/avalonbuild.com/Controllers/Api/GalleryController.cs
+++ src/avalonbuild.com/Controllers/Api/GalleryController.cs
@@ -27,7 +27,7 @@ namespace avalonbuild.com.Controllers.Api
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> Get()
         {
             var galleries = await _images.Galleries.Include(g => g.Images).ThenInclude(i => i.Image).ToListAsync();
 
@@ -41,10 +41,10 @@ namespace avalonbuild.com.Controllers.Api
             return Json(model);
         }
 
-        [HttpGet("{id}", Name = "GetGallery")]
-        public async Task<IActionResult> GetById(string id)
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
         {
-            var gallery = await _images.Galleries.Include(g => g.Images).SingleOrDefaultAsync(i => i.ID.ToString() == id);
+            var gallery = await _images.Galleries.Include(g => g.Images).ThenInclude(i => i.Image).SingleOrDefaultAsync(i => i.ID == id);
 
             if (gallery == null)
                 return NotFound();
@@ -52,9 +52,9 @@ namespace avalonbuild.com.Controllers.Api
             return Json(GalleryModelToViewModel(gallery));
         }
 
-        [HttpPost]
         [Authorize]
         [ValidateAntiForgeryToken]
+        [HttpPost]
         public async Task<IActionResult> Create([FromBody] ViewModels.Gallery gallery)
         {
             if (gallery == null)
@@ -78,13 +78,68 @@ namespace avalonbuild.com.Controllers.Api
 
             await _images.SaveChangesAsync();
 
-            string message = $"Gallery added successfully.";
-
-            return Json(message);
+            return Json("Gallery added successfully.");
         }
 
-        [HttpDelete("{id}")]
         [Authorize]
+        [ValidateAntiForgeryToken]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] ViewModels.Gallery gallery)
+        {
+            if (gallery == null)
+                return BadRequest("Error updating gallery.");
+
+            var dbGallery = await _images.Galleries.Include(g => g.Images).ThenInclude(i => i.Image).SingleOrDefaultAsync(i => i.ID == id);
+
+            dbGallery.ID = id;
+            dbGallery.Name = gallery.Name;
+            dbGallery.Title = gallery.Title;
+            dbGallery.Description = gallery.Description;
+
+            _images.Galleries.Update(dbGallery);
+
+            //remove images that are not in the new list
+            foreach (var image in dbGallery.Images.ToArray())
+            {
+                bool remove = true;
+
+                foreach (var i in gallery.Images)
+                {
+                    if (i.ID == image.ImageID)
+                        remove = false;
+                }
+
+                if (remove)
+                    dbGallery.Images.Remove(image);
+            }
+
+            // add images from the new list that are not in the old list
+            foreach (var image in gallery.Images)
+            {
+                bool add = true;
+
+                foreach (var i in dbGallery.Images)
+                {
+                    if (i.ImageID == image.ID)
+                        add = false;
+                }
+
+                if (add)
+                {
+                    var dbImage = await _images.Images.SingleOrDefaultAsync(i => i.ID == image.ID);
+
+                    if (dbImage != null)
+                        dbGallery.Images.Add(new GalleryImage { Image = dbImage});
+                }
+            }
+
+            await _images.SaveChangesAsync();
+
+            return Json("Gallery updated successfully.");        
+        }
+
+        [Authorize]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
             var gallery = await _images.Galleries.Include(g => g.Images).FirstOrDefaultAsync(i => i.ID == id);
@@ -107,20 +162,23 @@ namespace avalonbuild.com.Controllers.Api
                     Title = gallery.Title,
                     Description = gallery.Description
                 };
-
+                
                 foreach (var image in gallery.Images)
                 {
-                    var modelImage = new ViewModels.Image {
-                        ID = image.Image.ID,
-                        Name = image.Image.Name,
-                        Title = image.Image.Title,
-                        Description = image.Image.Description,
-                        FileName = image.Image.FileName
-                    };
+                    if (image.Image != null) {
 
-                    modelGallery.Images.Add(modelImage);
+                        var modelImage = new ViewModels.Image {
+                            ID = image.Image.ID,
+                            Name = image.Image.Name,
+                            Title = image.Image.Title,
+                            Description = image.Image.Description,
+                            FileName = image.Image.FileName
+                        };
+
+                        modelGallery.Images.Add(modelImage);
+                    }
                 }
-
+                
                 return modelGallery;
         }
     }
